cd4a3d0aaeba1e8f393e655ccaa6abfb2fdf69b8
Jonathan Tang <jonathan.d.tang@gmail.com>
2015-02-19 18:58:19 -0800

    Add C extension for collecting stats on a single parse.

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ This is composed of a small C library that runs Gumbo over a document with a
 custom allocator and tree-traversal, together with Python ctypes bindings for it
 and a driver script that runs it over a CommonCrawl segment and aggregates
 statistics.  Its purpose is to drive optimization decisions for Gumbo.
+
+## Building/running
+
+To build the C shared library:
+
+  gcc gumbo_stats.c -std=c99 -Wall -fpic -shared -o libgumbostats.so \
+    `pkg_config --cflags --libs gumbo`
+
+(We don't use automake or a build system because it's overkill for a single-file
+library that's only for information-gathering code.)
---
+++ gumbo_stats.c
@@ -0,0 +1,234 @@
+// Copyright 2015 Jonathan Tang. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stdlib.h"
+#include "string.h"
+#include "gumbo.h"
+#include "time.h"
+
+typedef struct {
+  unsigned int parse_time_us;
+  unsigned int traversal_time_us;
+
+  unsigned int allocations;
+  unsigned int frees;
+  unsigned int bytes_allocated;
+  unsigned int frees_during_parsing;
+
+  unsigned int nodes;
+  unsigned int elements;
+  unsigned int text;
+  unsigned int whitespace;
+  unsigned int cdata;
+  unsigned int comments;
+
+  unsigned int parser_inserted;
+  unsigned int reconstructed_formatting_element;
+  unsigned int adoption_agency_cloned;
+  unsigned int adoption_agency_moved;
+  unsigned int foster_parented;
+
+  GumboVector child_histogram;
+  GumboVector text_histogram;
+  GumboVector attribute_histogram;
+  GumboVector attribute_name_histogram;
+  GumboVector attribute_value_histogram;
+} GumboStats;
+
+typedef struct {
+  unsigned int children;
+  unsigned int text;
+  unsigned int attribute;
+  unsigned int attribute_name;
+  unsigned int attribute_value;
+} GumboMax;
+
+inline void set_max(unsigned int new_val, unsigned int* current) {
+  *current = new_val > *current ? new_val : *current;
+}
+
+inline void incr_histogram(unsigned int val, GumboVector* histogram) {
+  ++histogram->data[val];
+}
+
+// gumbo_vector_init isn't exposed publicly, and in any case we want to avoid
+// going through the allocator when we initialize our histograms so we don't
+// pollute our allocation stats.
+inline void stat_vector_init(unsigned int size, GumboVector* vector) {
+  vector->length = size + 1;
+  vector->data = malloc(sizeof(void*) * vector->length);
+}
+
+// Memory allocation functions
+
+static void* stat_collecting_malloc(void* userdata, size_t size) {
+  GumboStats* stats = (GumboStats*) userdata;
+  stats->allocations += 1;
+  stats->bytes_allocated += size;
+  return malloc(size);
+}
+
+static void stat_collecting_free(void* userdata, void* obj) {
+  GumboStats* stats = (GumboStats*) userdata;
+  stats->frees += 1;
+  free(obj);
+}
+
+// First tree traversal; this just collects maximum vector lengths for
+// children/attributes/text nodes so we can allocate histograms later.
+void find_max(GumboNode* node, GumboMax* max) {
+  switch(node->type) {
+    case GUMBO_NODE_DOCUMENT:
+      {
+        GumboDocument* doc = &node->v.document;
+        set_max(doc->children.length, &max->children);
+        set_max(strlen(doc->name), &max->text);
+        set_max(strlen(doc->public_identifier), &max->text);
+        set_max(strlen(doc->system_identifier), &max->text);
+        for (int i = 0; i < doc->children.length; ++i) {
+          find_max(doc->children.data[i], max);
+        }
+        return;
+      }
+    case GUMBO_NODE_ELEMENT:
+    case GUMBO_NODE_TEMPLATE:
+      {
+        GumboElement* elem = &node->v.element;
+        set_max(elem->children.length, &max->children);
+        set_max(elem->attributes.length, &max->attribute);
+        for (int i = 0; i < elem->attributes.length; ++i) {
+          GumboAttribute* attr = elem->attributes.data[i];
+          set_max(strlen(attr->name), &max->attribute_name);
+          set_max(strlen(attr->value), &max->attribute_value);
+        }
+        for (int i = 0; i < elem->children.length; ++i) {
+          find_max(elem->children.data[i], max);
+        }
+        return;
+      }
+    case GUMBO_NODE_TEXT:
+    case GUMBO_NODE_WHITESPACE:
+    case GUMBO_NODE_COMMENT:
+    case GUMBO_NODE_CDATA:
+      set_max(strlen(node->v.text.text), &max->text);
+      return;
+  }
+}
+
+// Second tree traversal; once we've had the GumboStats object fully
+// initialized, we recurse over the tree again to collect more detailed stats.
+void collect_stats(GumboNode* node, GumboStats* stats) {
+  ++stats->nodes;
+  if (node->parse_flags & GUMBO_INSERTION_BY_PARSER) {
+    ++stats->parser_inserted;
+  }
+  if (node->parse_flags & GUMBO_INSERTION_RECONSTRUCTED_FORMATTING_ELEMENT) {
+    ++stats->reconstructed_formatting_element;
+  }
+  if (node->parse_flags & GUMBO_INSERTION_ADOPTION_AGENCY_CLONED) {
+    ++stats->adoption_agency_cloned;
+  }
+  if (node->parse_flags & GUMBO_INSERTION_ADOPTION_AGENCY_MOVED) {
+    ++stats->adoption_agency_moved;
+  }
+  if (node->parse_flags & GUMBO_INSERTION_FOSTER_PARENTED) {
+    ++stats->foster_parented;
+  }
+  switch(node->type) {
+    case GUMBO_NODE_DOCUMENT:
+      {
+        GumboDocument* doc = &node->v.document;
+        incr_histogram(doc->children.length, &stats->child_histogram);
+        incr_histogram(strlen(doc->name), &stats->text_histogram);
+        incr_histogram(strlen(doc->public_identifier), &stats->text_histogram);
+        incr_histogram(strlen(doc->system_identifier), &stats->text_histogram);
+        for (int i = 0; i < doc->children.length; ++i) {
+          collect_stats(doc->children.data[i], stats);
+        }
+        return;
+      }
+    case GUMBO_NODE_ELEMENT:
+    case GUMBO_NODE_TEMPLATE:
+      {
+        GumboElement* elem = &node->v.element;
+        incr_histogram(elem->children.length, &stats->child_histogram);
+        incr_histogram(elem->attributes.length, &stats->attribute_histogram);
+
+        ++stats->elements;
+
+        for (int i = 0; i < elem->attributes.length; ++i) {
+          GumboAttribute* attr = elem->attributes.data[i];
+          incr_histogram(strlen(attr->name), &stats->attribute_name_histogram);
+          incr_histogram(strlen(attr->value), &stats->attribute_value_histogram);
+        }
+        for (int i = 0; i < elem->children.length; ++i) {
+          collect_stats(elem->children.data[i], stats);
+        }
+        return;
+      }
+    case GUMBO_NODE_TEXT:
+      ++stats->text;
+      incr_histogram(strlen(node->v.text.text), &stats->text_histogram);
+      return;
+    case GUMBO_NODE_WHITESPACE:
+      ++stats->whitespace;
+      incr_histogram(strlen(node->v.text.text), &stats->text_histogram);
+      return;
+    case GUMBO_NODE_COMMENT:
+      ++stats->comments;
+      incr_histogram(strlen(node->v.text.text), &stats->text_histogram);
+      return;
+    case GUMBO_NODE_CDATA:
+      ++stats->cdata;
+      incr_histogram(strlen(node->v.text.text), &stats->text_histogram);
+      return;
+  }
+}
+
+void parse(const char* input, GumboStats* stats) {
+  memset(stats, 0, sizeof(GumboStats));
+  GumboOptions options;
+  options.allocator = stat_collecting_malloc;
+  options.deallocator = stat_collecting_free;
+  options.userdata = stats;
+  
+  clock_t start_time = clock();
+  GumboOutput* output = gumbo_parse_with_options(
+    &options, input, strlen(input));
+  clock_t end_time = clock();
+  stats->parse_time_us = 1000000 * (end_time - start_time) / CLOCKS_PER_SEC;
+
+  GumboMax max;
+  start_time = clock();
+  find_max(output->document, &max);
+  end_time = clock();
+  stats->traversal_time_us =
+      1000000 * (end_time - start_time) / CLOCKS_PER_SEC;
+
+  stat_vector_init(max.children, &stats->child_histogram);
+  stat_vector_init(max.text, &stats->text_histogram);
+  stat_vector_init(max.attribute, &stats->attribute_histogram);
+  stat_vector_init(max.attribute_name, &stats->attribute_name_histogram);
+  stat_vector_init(max.attribute_value, &stats->attribute_value_histogram);
+
+  collect_stats(output->document, stats);
+}
+
+#ifdef __cplusplus
+}
+#endif
